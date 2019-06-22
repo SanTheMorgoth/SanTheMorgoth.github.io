d726229166eacda325b63863e2688dd7b0ddc5db
Sundar <sdrmec@gmail.com>
2019-06-22 12:14:09 +0530

    sw

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,7 +1,10 @@
 const cacheName = 'v1';
 
 const cacheAssests = [
-    '/images/flower.jpeg'
+    '/images/flower.jpeg',
+    '/images/sea.jpeg',
+    '/index.html',
+    '/about.html'
 ];
 
 //call install event
@@ -11,6 +14,7 @@ self.addEventListener('install', e => {
         caches.open(cacheName)
         .then(cache => {
             console.log('SW::CACHING FILES');
+            console.log(cache, "ll");
             cache.addAll(cacheAssests);
         })
         .then(() => self.skipWaiting())
@@ -20,4 +24,29 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
     console.log('SW::ACTIVATED');
+    e.waitUntil(
+        /* Delete the previous version of caches if exists in the cache storage */
+        caches.keys().then((cacheNames) => {
+            cacheNames.map(cache => {
+                if (cache !== cacheName) {
+                    caches.delete(cache);
+                }
+            });
+        })
+    );
+});
+
+
+self.addEventListener('fetch', e => {
+    console.log('SW::FETCH INTERCEPTOR');
+    e.respondWith(
+        fetch(e.request).then(res => {
+            const resClone = res.clone();
+            caches.open(cacheName)
+            .then(cache => {
+                cache.put(e.request, resClone);
+            });
+            return res;
+        })
+    );
 });
